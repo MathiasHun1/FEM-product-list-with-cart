6d515191a9e55155e5e745d697415d54cab55fe6
snus4me <mathias.hun@gmail.com>
2025-02-08 18:28:18 +0100

    scroll up view when confirmed order

--- src/components/Button-confirm/ButtonConfirm.jsx
+++ src/components/Button-confirm/ButtonConfirm.jsx
@@ -1,8 +1,13 @@
 import styles from './ButtonConfirm.module.css';
 
 const ButtonConfirm = ({ text = '', setModalOpen, modalOpen }) => {
+  const handleClick = () => {
+    setModalOpen(!modalOpen);
+    window.scrollTo({ top: 0, behavior: 'instant' });
+  };
+
   return (
-    <button className={styles.button} onClick={() => setModalOpen(!modalOpen)}>
+    <button className={styles.button} onClick={handleClick}>
       {text}
     </button>
   );
